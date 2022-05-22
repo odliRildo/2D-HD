6e0256f4f15cc4250188c49bbef075dec33acb09
odliRildo <rildo.barbosafilho@ufpe.br>
2022-05-22 01:02:30 -0300

    Ground and stairs
    implemented poorly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,17 @@ public class PlayerController : MonoBehaviour
 
     public Animator animator;
 
+    public GameObject stepRayLower;
+    public GameObject stepRayUpper;
+    public float stepHeight ;
+    public float stepSmooth ;
 
+    public float distToGround = 1f;
+    public float togroundspeed;
     private void Awake(){
         playerControls = new PlayerInputActions();
+        stepRayUpper.transform.position = new Vector3(stepRayUpper.transform.position.x,stepHeight,stepRayUpper.transform.position.z);
+
     }
 
 
@@ -48,10 +56,61 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate(){
         rb.velocity = new Vector3( moveDirection.x*moveSpeed , 0, moveDirection.y*moveSpeed );
+
+        stepClimb();
+        if(!isGrounded()){
+            rb.velocity += new Vector3(0,-togroundspeed,0);
+            Debug.Log("Not Grounded");
+        }
     }
 
     private void Fire(InputAction.CallbackContext context){
         animator.SetTrigger("Attack");
         
     }
+
+    void stepClimb()
+    {
+         RaycastHit hitLower;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.1f))
+        {
+            RaycastHit hitUpper;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.2f))
+            {
+                rb.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+
+         RaycastHit hitLower45;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f,0,1), out hitLower45, 0.1f))
+        {
+
+            RaycastHit hitUpper45;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f,0,1), out hitUpper45, 0.2f))
+            {
+                rb.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+
+        RaycastHit hitLowerMinus45;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f,0,1), out hitLowerMinus45, 0.1f))
+        {
+
+            RaycastHit hitUpperMinus45;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f,0,1), out hitUpperMinus45, 0.2f))
+            {
+                rb.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+    }
+
+    bool isGrounded(){
+        if(Physics.Raycast(transform.position,Vector3.down, distToGround + 0.1f)){
+            return false;
+        }
+        return true;
+    }
+
+
+
 }
